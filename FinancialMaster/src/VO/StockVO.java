d028094c2e70921e6bb6a410915c8ea125e32deb
ldc14 <ldc14@software.nju.edu.cn>
2016-03-06 20:54:50 +0800

    bl层装，测试界面

--- FinancialMaster/src/VO/StockVO.java
+++ FinancialMaster/src/VO/StockVO.java
@@ -1,12 +1,65 @@
 package VO;
 
 public class StockVO {
-	String id;
-	String name;
-	double opening_price;
-	double closing_price;
-	double max_price;
-	double min_price;
-	int trading_amout;
-	double  trading_money;
+//	日期、开盘价、最高价、收盘价、最低价、交易量、交易金额
+	String id;//日期
+	double opening_price;//开盘价
+	double closing_price;//收盘价
+	double max_price;//最高价
+	double min_price;//最低价
+	int trading_amout;//交易量
+	double  trading_money;//交易金额
+	String[][] history_data;//历史数据
+	
+
+	
+	public StockVO(String id, double opening_price, double closing_price,
+			double max_price, double min_price, int trading_amout,
+			double trading_money, String[][] history_data) {
+		super();
+		this.id = id;
+		this.opening_price = opening_price;
+		this.closing_price = closing_price;
+		this.max_price = max_price;
+		this.min_price = min_price;
+		this.trading_amout = trading_amout;
+		this.trading_money = trading_money;
+		this.history_data = history_data;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public double getOpening_price() {
+		return opening_price;
+	}
+
+	public double getClosing_price() {
+		return closing_price;
+	}
+
+	public double getMax_price() {
+		return max_price;
+	}
+
+	public double getMin_price() {
+		return min_price;
+	}
+
+	public int getTrading_amout() {
+		return trading_amout;
+	}
+
+	public double getTrading_money() {
+		return trading_money;
+	}
+
+	public String[][] getHistory_data() {
+		return history_data;
+	}
+	public void setHistory_data(String[][] history_data) {
+		this.history_data = history_data;
+	}
+
 }
--- FinancialMaster/src/businesslogic/stockcheckbl/StockListBL.java
+++ FinancialMaster/src/businesslogic/stockcheckbl/StockListBL.java
@@ -8,13 +8,16 @@ public class StockListBL implements StockListBLService {
 	@Override
 	public String[][] getStockList() {
 //		股票代码、开盘价、最高价、收盘价、最低价、交易量、交易金额
-		String[][] test={{"600000","27.65","33.66","31.23","26.55","10240000","1000000000"}};
-		return null;
+		String[][] test={{"sh600000","27.65","33.66","31.23","26.55","10240000","1000000000"}
+		,{"sh600001","27.65","33.66","31.23","26.55","10240000","1000000000"},
+		{"sh600002","27.65","33.66","31.23","26.55","10240000","1000000000"},
+		{"sh600003","27.65","33.66","31.23","26.55","10240000","1000000000"}};
+		return test;
 	}
 
 	@Override
 	public String[][] updateStockList(String key) {
-
+        
 		return null;
 	}
 
