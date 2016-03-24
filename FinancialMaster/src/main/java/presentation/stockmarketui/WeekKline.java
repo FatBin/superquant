8a657f3ce2ce8e71f068522da0525662b354e785
lhs14 <lhs14@software.nju.edu.cn>
2016-03-24 09:02:43 +0800

    k线图优化

--- FinancialMaster/src/main/java/presentation/stockmarketui/WeekKline.java
+++ FinancialMaster/src/main/java/presentation/stockmarketui/WeekKline.java
@@ -12,9 +12,7 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.DateTickMarkPosition;
-import org.jfree.chart.axis.DateTickUnit;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.NumberTickUnit;
 import org.jfree.chart.axis.SegmentedTimeline;
 import org.jfree.chart.plot.CombinedDomainXYPlot;
 import org.jfree.chart.plot.XYPlot;
@@ -40,7 +38,6 @@ public class WeekKline extends JPanel {
 	Date endDate;
 	private StockMarketBLService stockMarketBL = new StockMarketBL();
 
-	@SuppressWarnings("deprecation")
 	public WeekKline() {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		double highValue = Double.MIN_VALUE; // 设置K线数据当中的最大值
@@ -123,19 +120,14 @@ public class WeekKline extends JPanel {
 		candlestickRender.setUpPaint(new Color(179, 8, 77));// 设置股票上涨的K线图颜色
 		candlestickRender.setDownPaint(new Color(25, 155, 83));// 设置股票下跌的K线图颜色
 		DateAxis x1Axis = new DateAxis();// 设置x轴，也就是时间轴
-		x1Axis.setAutoRange(false);// 设置不采用自动设置时间范围
 		
 		x1Axis.setRange(startDate, endDate);// 设置时间范围，注意时间的最大值要比已有的时间最大值要多一天
 		x1Axis.setTimeline(SegmentedTimeline.newMondayThroughFridayTimeline());// 设置时间线显示的规则，用这个方法就摒除掉了周六和周日这些没有交易的日期(很多人都不知道有此方法)，使图形看上去连续
-		x1Axis.setAutoTickUnitSelection(false);// 设置不采用自动选择刻度值
 		x1Axis.setTickMarkPosition(DateTickMarkPosition.MIDDLE);// 设置标记的位置
 		x1Axis.setStandardTickUnits(DateAxis.createStandardDateTickUnits());// 设置标准的时间刻度单位
-		x1Axis.setTickUnit(new DateTickUnit(DateTickUnit.DAY, 15));// 设置时间刻度的间隔，一般以周为单位
 		x1Axis.setDateFormatOverride(new SimpleDateFormat("yyyy-MM-dd"));
 		NumberAxis y1Axis = new NumberAxis();// 设定y轴，就是数字轴
-		y1Axis.setAutoRange(false);// 不不使用自动设定范围
-		y1Axis.setRange(minValue * 0.9, highValue * 1.1);// 设定y轴值的范围，比最低值要低一些，比最大值要大一些，这样图形看起来会美观些
-		y1Axis.setTickUnit(new NumberTickUnit((highValue * 1.1 - minValue * 0.9) / 10));// 设置刻度显示的密度
+		y1Axis.setRange(minValue * 0.98, highValue * 1.02);// 设定y轴值的范围，比最低值要低一些，比最大值要大一些，这样图形看起来会美观些
 		XYPlot plot1 = new XYPlot(seriesCollection, x1Axis, y1Axis, candlestickRender);// 设置画图区域对象
 
 		XYBarRenderer xyBarRender = new XYBarRenderer() {
@@ -149,9 +141,7 @@ public class WeekKline extends JPanel {
 		};
 		xyBarRender.setMargin(0.1);// 设置柱形图之间的间隔
 		NumberAxis y2Axis = new NumberAxis();// 设置Y轴，为数值,后面的设置，参考上面的y轴设置
-		y2Axis.setAutoRange(false);
 		y2Axis.setRange(min2Value * 0.9, high2Value * 1.1);
-		y2Axis.setTickUnit(new NumberTickUnit((high2Value * 1.1 - min2Value * 0.9) / 4));
 		XYPlot plot2 = new XYPlot(timeSeriesCollection, null, y2Axis, xyBarRender);// 建立第二个画图区域对象，主要此时的x轴设为了null值，因为要与第一个画图区域对象共享x轴
 
 		CombinedDomainXYPlot combineddomainxyplot = new CombinedDomainXYPlot(x1Axis);// 建立一个恰当的联合图形区域对象，以x轴为共享轴
