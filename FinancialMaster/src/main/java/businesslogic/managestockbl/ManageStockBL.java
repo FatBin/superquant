03637643bfccdc899d19e44cba2540f25e0dee2e
ldc14 <ldc14@software.nju.edu.cn>
2016-04-11 19:21:34 +0800

    增加关注与取消关注时更新工厂

--- FinancialMaster/src/main/java/businesslogic/managestockbl/ManageStockBL.java
+++ FinancialMaster/src/main/java/businesslogic/managestockbl/ManageStockBL.java
@@ -6,18 +6,25 @@ import data.manageStockData.ManageStockData;
 import dataservice.manageStockService.manageStockDataService;
 import ENUM.ManageState;
 import ENUM.attentionState;
+import businesslogic.factory.InitFactory;
 import businesslogicservice.managestockblservice.ManageStockBLService;
 
 public class ManageStockBL implements ManageStockBLService {
 	manageStockDataService msds=new ManageStockData();
 	@Override
 	public ManageState addStock(String id) {
-		return msds.addStock(id);
+		ManageState result=msds.addStock(id);
+		InitFactory factory=InitFactory.getFactory();
+		factory.update();
+		return result;
 	}
 
 	@Override
 	public ManageState deleteStock(String id) {
-		return msds.deleteStock(id);
+		ManageState result=msds.deleteStock(id);
+		InitFactory factory=InitFactory.getFactory();
+		factory.update();
+		return result;
 	}
 
 	@Override
