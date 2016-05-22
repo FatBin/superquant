e98bc499e9d106ce85595ef3fcefc79ec9585f77
lhs14 <lhs14@software.nju.edu.cn>
2016-05-22 20:59:43 +0800

    测试注册到数据库

--- FinancialMaster/src/main/java/servlet/login/LoginServlet.java
+++ FinancialMaster/src/main/java/servlet/login/LoginServlet.java
@@ -19,39 +19,43 @@ import ENUM.ManageState;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserVO  user=new UserVO();
-		String username=request.getParameter("username");
-		String password=request.getParameter("password");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		UserVO user = new UserVO();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 		user.setUsername(username);
 		user.setPassword(password);
-		LoginBLService loginbl=new LoginBL();
-		ManageState LoginResule =loginbl.login(user);
-		request.getSession().setAttribute("LoginResult", LoginResule);
-		if(LoginResule==ManageState.Succeed){			 
-		    request.getSession().setAttribute("User", user);
+		LoginBLService loginbl = new LoginBL();
+		ManageState LoginResult = loginbl.login(user);
+		request.getSession().setAttribute("LoginResult", LoginResult);
+		if (LoginResult == ManageState.Succeed) {
+			request.getSession().setAttribute("User", user);
 		}
-		
+
 	}
 
 }
--- FinancialMaster/src/main/java/servlet/login/RegisterServlet.java
+++ FinancialMaster/src/main/java/servlet/login/RegisterServlet.java
@@ -7,36 +7,59 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ENUM.ManageState;
+import VO.UserVO;
+import businesslogic.loginbl.LoginBL;
+import businesslogicservice.loginblservice.LoginBLService;
+
 /**
  * Servlet implementation class RegisterServlet
  */
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public RegisterServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-		System.out.println("success");
+		System.out.println("register contact success");
+
+		UserVO user = new UserVO();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		user.setUsername(username);
+		user.setPassword(password);
+		LoginBLService loginbl = new LoginBL();
+		ManageState RegistResult = loginbl.add(user);
+		
+		request.getSession().setAttribute("RegistResult", RegistResult);
+		if (RegistResult == ManageState.Succeed) {
+			request.getSession().setAttribute("User", user);
+			
+			request.getRequestDispatcher("../webapp/Web_Pages/HomePage.jsp").forward(request, response);
+		}
 	}
 
 }
