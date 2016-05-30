69c0d5047aefbb5cfbde29849ebef03fbf08c8ae
lhs14 <lhs14@software.nju.edu.cn>
2016-05-30 22:50:11 +0800

    实时搜索增加鼠标监听

--- FinancialMaster/src/main/webapp/js/searchHint.js
+++ FinancialMaster/src/main/webapp/js/searchHint.js
@@ -43,6 +43,8 @@ function showHint(str) {
 			for (var i = 0; i < datalen; i++) {
 				var li = document.createElement("li");
 				li.innerHTML = data[i];
+				li.setAttribute("onmouseover", "search_mouseIn(" + i + ")");
+				li.setAttribute("onmouseout", "search_mouseOut(" + i + ")");
 				div.appendChild(li);
 			}
 		}
@@ -50,18 +52,20 @@ function showHint(str) {
 		var uls = div.getElementsByTagName("li");
 		var rowcounts = uls.length;
 		if (event.keyCode == 38) {
+
+			for (var i = 0; i < uls.length; i++) {
+				uls[i].style.backgroundColor = "rgb(235, 235, 235)";
+			}
 			if (rowpos > 0) {
-				if (rowpos < rowcounts) {
-					uls[rowpos].style.backgroundColor = "rgb(235, 235, 235)";
-				}
 				rowpos--;
 			}
 			uls[rowpos].style.backgroundColor = "rgb(127, 127, 127)";
 		} else if (event.keyCode == 40) {
+
+			for (var i = 0; i < uls.length; i++) {
+				uls[i].style.backgroundColor = "rgb(235, 235, 235)";
+			}
 			if (rowpos < rowcounts - 1) {
-				if (rowpos > -1) {
-					uls[rowpos].style.backgroundColor = "rgb(235, 235, 235)";
-				}
 				rowpos++;
 			}
 			uls[rowpos].style.backgroundColor = "rgb(127, 127, 127)";
@@ -71,4 +75,19 @@ function showHint(str) {
 			rowpos = -1;
 		}
 	}
+}
+
+function search_mouseIn(pos) {
+	var uls = document.getElementById("searchHint").getElementsByTagName("li");
+	for (var i = 0; i < uls.length; i++) {
+		uls[i].style.backgroundColor = "rgb(235, 235, 235)";
+	}
+	uls[pos].style.backgroundColor = "rgb(127, 127, 127)";
+	rowpos = pos;
+}
+
+function search_mouseOut(pos) {
+	var uls = document.getElementById("searchHint").getElementsByTagName("li");
+	uls[pos].style.backgroundColor = "rgb(235, 235, 235)";
+	rowpos = -1;
 }
