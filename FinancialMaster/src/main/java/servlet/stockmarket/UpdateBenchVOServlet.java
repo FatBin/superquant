ba7a6dd4c0d06642d8662d05f2b2eaed8b988592
ldc14 <ldc14@software.nju.edu.cn>
2016-06-09 22:13:49 +0800

    大盘最新数据

--- FinancialMaster/src/main/java/servlet/stockmarket/UpdateBenchVOServlet.java
+++ FinancialMaster/src/main/java/servlet/stockmarket/UpdateBenchVOServlet.java
@@ -1,12 +1,15 @@
 package servlet.stockmarket;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
 
 import VO.BenchVO;
 import web.bl.benchImpl.BenchImpl;
@@ -43,8 +46,28 @@ public class UpdateBenchVOServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		 BenchVO sv=(BenchVO)request.getSession().getAttribute("BenchMarket");
+		 
+			String data="[{'status':"+sv.getStatus()+
+					",'time':"+sv.getTime()+
+					",'now':"+sv.getNow()+
+					",'rise_fall_price':"+sv.getRise_fall_price()+
+					",'rise_fall_percent':"+sv.getRise_fall_percent()+
+					",'high':"+sv.getHigh()+
+					",'low':"+sv.getLow()+
+					",'open':"+sv.getOpen()+
+					",'yesterday_close':"+sv.getYesterday_close()+
+					",'price':"+sv.getPrice()+
+					",'volume':"+sv.getVolume()+
+					",'rise_company':"+sv.getRise_company()+
+					",'fall_company':"+sv.getFall_company()+
+					",'company':"+sv.getCompany()+ "}]";
+
+			JSONArray json = new JSONArray(data);
+			PrintWriter out = response.getWriter();
+			out.println(json);
+			out.flush();
+			out.close();
 	}
 
 }
