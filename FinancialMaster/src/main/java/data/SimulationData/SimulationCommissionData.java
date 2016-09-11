e3a4e4798164571fb40fa53d192a30381e28a2c6
lxbin14 <18813632558@163.com>
2016-09-11 19:34:31 +0800

    第三张表接口实现

--- FinancialMaster/src/main/java/data/SimulationData/SimulationCommissionData.java
+++ FinancialMaster/src/main/java/data/SimulationData/SimulationCommissionData.java
@@ -1,14 +1,37 @@
 package data.SimulationData;
 
+import java.util.ArrayList;
+
+import DAO.DAOimpl.SimulationcommissionDaoImpl;
+import DAO.dao.SimulationcommissionDao;
 import DAO.pojo.Simulation;
+import DAO.pojo.Simulationcommission;
 import dataservice.SimulationDataService.SimulationCommissionDataService;
 
 public class SimulationCommissionData implements SimulationCommissionDataService{
-
+	SimulationcommissionDao SimulationcommissionDaoImpl;
 	@Override
-	public int persist(Simulation simulation) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int persist(Simulationcommission simulationcommission) {
+		SimulationcommissionDaoImpl=new SimulationcommissionDaoImpl();
+		return SimulationcommissionDaoImpl.persist(simulationcommission);
 	}
 
+	@Override
+	public boolean remove(int id) {
+		SimulationcommissionDaoImpl=new SimulationcommissionDaoImpl();
+		return SimulationcommissionDaoImpl.remove(id);
+	}
+
+	@Override
+	public ArrayList<Simulationcommission> getAllSimulationcommissions() {
+		// TODO Auto-generated method stub
+		SimulationcommissionDaoImpl=new SimulationcommissionDaoImpl();
+		ArrayList<Simulationcommission> arrayList=new ArrayList<>();
+		if (SimulationcommissionDaoImpl.getAllSimualtioncommissions()!=null) {
+			arrayList=(ArrayList<Simulationcommission>) SimulationcommissionDaoImpl.getAllSimualtioncommissions();
+		}
+		return arrayList;
+	}
+
+	
 }
